8fa007f775509da5ba0618695e3ae8352dfbd170
aryanbarde80 <aryanbarde80@gmail.com>
2025-07-06 04:20:59 +0530

    cron job implemented

--- src/server.js
+++ src/server.js
@@ -2,13 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import job from "./config/cron.js";
 
 import transactionsRoute from "./routes/transactionsRoutes.js";
 
 dotenv.config();
 
 const app = express();
+if(process.env.NODE_ENV==="production") job.start();
 const PORT = process.env.PORT || 5001;
+
 
 //made middleware
 app.use(rateLimiter);
@@ -17,8 +20,8 @@ app.use(express.json());
 
 
 // health check route 
-app.get("/health", (req, res) => {
-  res.send("It's working");
+app.get("/api/health", (req, res) => {
+  res.status(200).json({status:"ok"});
 });
 
 console.log("my port:", process.env.PORT);
